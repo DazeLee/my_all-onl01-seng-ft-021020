21783f5af485a83ed7dabfda4f0cd123399a78f4
Daisy Olmo-Rivera <dazelee@yahoo.com>
2020-02-14 20:53:42 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,11 +2,12 @@ require 'pry'
 
 def my_all?(collection)
   i=0 
-  # arr =[]
+  
   while i < collection.length 
   if yield (collection[i])== false 
     return false 
   end 
   i +=1 
+  return true
 
 end
