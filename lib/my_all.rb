e3b304a682adba24534bb4437fafdc5577eb9900
Daisy Olmo-Rivera <dazelee@yahoo.com>
2020-02-14 21:22:14 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,17 +2,11 @@
 
 def my_all?(collection)
   i = 0 
-  # arr = []
   while i < collection.length 
   if yield (collection [i]) == false 
     return false 
   end 
   i += 1 
 end
-# if arr.include?(false)
-# return false
-#else 
-#  return true 
-# end 
   return true
 end
